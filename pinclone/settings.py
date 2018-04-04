3a7e42e93ccb1e81b26f9b7ab32a1a75969d7979
Ayush Rawat <aysh007rwt@gmail.com>
2018-04-04 22:14:08 +0530

    added homepage about and base_layout

--- pinclone/settings.py
+++ pinclone/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'pinclone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
