bbc586d59fd1d48dbd57ec37b470982aeeab752f
Amit <amit.vrma11111@gmail.com>
2018-04-14 17:30:56 +0530

    model first commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Preferences(models.Model):
+	name = models.CharField(max_length = 50)
+
+
+class Images(models.Model):
+	name = models.CharField(max_length = 400)
+	photo = models.ImageField(null=True,blank=True)
+	tag = models.ForeignKey(Preferences,on_delete = models.CASCADE)
+
+
+sdkhk
